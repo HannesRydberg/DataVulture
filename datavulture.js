b94f205dc5bc284cd37df992065c1cb836b8dd7b
Hannes Rydberg <hannes.rydberg@omegapoint.se>
2018-01-15 16:31:52 +0100

    add storage of coordinates (or approximated coordinates) of tweet origin

--- datavulture.js
+++ datavulture.js
@@ -28,6 +28,12 @@ fs.readFile("tokens.txt", "UTF-8", ((err, data) => {
     startClient(result)
 }))
 
+var coordinateAVG = (coord1, coord2) => {
+  var long = coord1[0] + ((coord2[0] - coord1[0])/2)
+  var lat = coord1[1] + ((coord2[1] - coord1[1])/2)
+  return([long, lat])
+}
+
 var startClient = (credentials) => {
   console.log(startmessage)
 
@@ -41,20 +47,34 @@ var startClient = (credentials) => {
       console.log(err)
     }
   }))
-  
+
   var db = new sqlite3.Database('vultureNest.db')
-  db.run("CREATE TABLE if not exists tweet_info (id TEXT, query TEXT, place TEXT, country TEXT, time TEXT, text TEXT)")
+  db.run("CREATE TABLE if not exists tweet_info (id TEXT NOT NULL PRIMARY KEY, query TEXT, longitude TEXT, latitude TEXT, place TEXT, country TEXT, time TEXT, text TEXT)")
   
   client.stream("statuses/filter", {track: query}, ((stream) => {   
     stream.on("data", ((tweet) => {
-      if(tweet.place){
+      if(tweet.coordinates || tweet.place){
         var text = tweet.text.replace(/['"]+/g, '')
         for(var i = 0; i < subQueries.length; i++){
           if(text.indexOf(subQueries[i]) > -1){
-  
+
+            var coordinates
+            if(tweet.coordinates){
+              coordinates = tweet.coordinates.coordinates
+            }else{
+              coordinates = tweet.place.bounding_box.coordinates[0]
+              var pointA = coordinates[0].toString().split(",")
+              var pointB = coordinates[2].toString().split(",")
+
+              coordinates = coordinateAVG([Number(pointA[0]), Number(pointA[1])],
+                [Number(pointB[0]), Number(pointB[1])])
+            }
+
             db.run("INSERT INTO tweet_info VALUES ('"
             + JSON.stringify(tweet.id) + "','"
             + subQueries[i] + "','"
+            + coordinates[0] + "','"
+            + coordinates[1] + "','"
             + JSON.stringify(tweet.place.full_name) + "','"
             + JSON.stringify(tweet.place.country) + "','"
             + Date.now() + "','" 
@@ -64,9 +84,10 @@ var startClient = (credentials) => {
                 console.log(err)
               }
             }))
-            // console.log(text)
+
             console.log(":::Search Query hit: " + subQueries[i])
             console.log("ID: " + JSON.stringify(tweet.id))
+            console.log("Coordinates: " + JSON.stringify(coordinates))
             console.log("Place: " + JSON.stringify(tweet.place.full_name))
             console.log("Country: " + JSON.stringify(tweet.place.country))
             console.log("Time: " + Date.now()) 
@@ -82,21 +103,3 @@ var startClient = (credentials) => {
     }))
   }))
 }
-
-
-
-
-
-
-
-
-
-var count = ((count) => {
-  readline.clearLine()
-  readline.cursorTo(rl.output, 0)
-  rl.write("Count:" + count)
-})
-
-
-
-
